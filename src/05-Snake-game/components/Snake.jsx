d5007e952337e6ecf1991be7fbc6506d25353857
Blacklion567 <naviedaj567@gmail.com>
2024-01-03 19:16:45 +0800

    05-Snake-game

---
+++ src/05-Snake-game/components/Snake.jsx
@@ -0,0 +1,23 @@
+
+const Snake = ({ snake }) => {
+  return (
+    <div>
+      {snake.map((box) => (
+        <div
+          style={{
+            width: "15px",
+            height: "15px",
+            backgroundColor: "#e7da3d",
+            margin: "5px",
+            position: "absolute",
+            left: `${box.x}%`,
+            top: `${box.y}%`,
+            zIndex: 1,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Snake;
