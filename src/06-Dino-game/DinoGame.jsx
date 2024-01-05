541253d548e8fcfae6026ce750ed59998bcf8a5d
Blacklion567 <naviedaj567@gmail.com>
2024-01-05 18:40:02 +0800

    06-Dino-game

---
+++ src/06-Dino-game/DinoGame.jsx
@@ -0,0 +1,12 @@
+import Dino from "./components/Dino/Dino";
+import './App.css';
+
+const DinoGame = () => {
+  return (
+    <div className="App">
+      <Dino />
+    </div>
+  );
+};
+
+export default DinoGame;
