9e05eecb8f25bd15703b5edbdb2c63317acc90ae
Blacklion567 <naviedaj567@gmail.com>
2024-01-30 22:53:47 +0800

    08-Tag-Input

---
+++ src/08-Tag-Input/EightProject.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import "./App.css";
+import TagInput from "./TagInput";
+
+const EightProject = () => {
+  return (
+    <>
+      <TagInput />
+    </>
+  );
+};
+
+export default EightProject;
