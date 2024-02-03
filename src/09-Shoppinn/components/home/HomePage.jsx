f5b5a67e7775ec178a402a5005d6f2175e206285
Blacklion567 <naviedaj567@gmail.com>
2024-02-03 18:53:58 +0800

    09-Shoppinn

---
+++ src/09-Shoppinn/components/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import items from "../../mockData/items.json";
+import ItemList from "../itemList/ItemList";
+
+function HomePage() {
+  return (
+    <section>
+      <ItemList items={items} />
+    </section>
+  );
+}
+
+export default HomePage;
